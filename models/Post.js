b616e2b614be868184a3dfd0990ce5054a78165e
unknown <diegoalexisaguirre2@gmail.com>
2023-05-15 21:55:29 -0300

    Apis rest Social

---
+++ models/Post.js
@@ -0,0 +1,24 @@
+const mongosse = require("mongoose");
+
+const PostSchema = new mongosse.Schema(
+  {
+    userId: {
+      type: String,
+      require: true,
+    },
+    desc: {
+      type: String,
+      max: 500,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongosse.model("Post", PostSchema);
